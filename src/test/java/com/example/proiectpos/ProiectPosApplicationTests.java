f26ee21bc58cd8c300f104847e1d6a867267300b
Ivanov Alexandru <alexandruivanov8@gmail.com>
2023-10-24 11:57:25 +0300

    initial commit

---
+++ src/test/java/com/example/proiectpos/ProiectPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proiectpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
